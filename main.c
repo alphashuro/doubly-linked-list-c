8ee6bc0852c4d9eb52b0562c0405d09c1a7e7096
Alpha Shuro <alphashuro@gmail.com>
2023-07-17 08:55:20 +0200

    fix main

--- main.c
+++ main.c
@@ -1,24 +1,30 @@
-#include "doubly_linked_list.h"
+#include "doubly_linked_list/doubly_linked_list.h"
+#include <stdio.h>
 
 int main() {
-  DoublyLinkedList* list = dll_newList();
+  DoublyLinkedList *list = dll_newList();
 
-  append(list, "second");
-  append(list, "third");
-  append(list, "fourth");
-  prepend(list, "first");
-  prepend(list, "SOMETHING");
-  append(list, "SOMETHING");
+  dll_append(list, "second");
+  dll_append(list, "third");
+  dll_append(list, "fourth");
+  dll_prepend(list, "first");
+  dll_prepend(list, "SOMETHING");
+  dll_append(list, "SOMETHING");
+  dll_insert(list, 3, "another thing");
 
-  print(list);
+  printf("%s\n", dll_toString(list));
 
-  remove_head(list);
+  dll_remove_at(list, 0);
 
-  print(list);
+  printf("%s\n", dll_toString(list));
 
-  remove_tail(list);
+  dll_remove_at(list, -1);
 
-  print(list);
+  printf("%s\n", dll_toString(list));
+
+  Node *item = dll_get(list, 2);
+
+  printf("%s\n", item->value);
 
   return 0;
 }
